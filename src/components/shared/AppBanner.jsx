2807a678fe68972e81640df5f6a785c735e65422
nNikiforos <nikiforos.nik96@gmail.com>
2024-10-21 12:04:34 +0300

    new resume

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -1,7 +1,7 @@
 import { FiArrowDownCircle } from "react-icons/fi";
 import developerImg from "../../images/developer-dark.jpeg";
 import { motion } from "framer-motion";
-import resume from "../../assets/resume-me.pdf";
+import resume from "../../assets/ResumeNikos.pdf";
 
 const AppBanner = () => {
   return (
